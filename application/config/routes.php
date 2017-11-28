48cb2733a239ea1dc5dc7ade959e53f6360a2b00
Erdem Erciyas <erdem_kirsehir_40@hotmail.com>
2017-11-28 14:26:52 +0200

    tutorial afgemaakt

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,9 +7,9 @@ $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 
 
-// dit zorgt ervoor dat de "Home" naar "About" veranderd
 	
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,1 @@
-Hello World!
+welkom bij home.php
